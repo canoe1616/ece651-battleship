d38f3a0e1ba6a17a74d7d4ee44c0a72ab94861a2
Yezhou Chen <Yezhou.chen@duke.edu>
2022-01-18 20:24:29 -0500

    adding task6

---
+++ app/src/main/java/edu/duke/yc407/battleship/BasicShip.java
@@ -0,0 +1,42 @@
+package edu.duke.yc407.battleship;
+
+public class BasicShip implements Ship<Character> {
+  private final Coordinate myLocation;
+
+  public BasicShip(Coordinate c){
+    this.myLocation =c;
+  }
+  
+  @Override
+  public boolean occupiesCoordinates(Coordinate where) {
+    // TODO Auto-generated method stub
+    return where.equals(myLocation);
+  }
+
+  @Override
+  public boolean isSunk() {
+    // TODO Auto-generated method stub
+    return false;
+  }
+
+  @Override
+  public void recordHitAt(Coordinate where) {
+    // TODO Auto-generated method stub
+    
+  }
+
+  @Override
+  public boolean wasHitAt(Coordinate where) {
+    // TODO Auto-generated method stub
+    return false;
+  }
+
+  @Override
+  public Character getDisplayInfoAt(Coordinate where) {
+    // TODO Auto-generated method stub
+    return 's';
+  }
+
+  
+
+}

74c3e7311e5a1f3686974373050cb54455797c5d
Yezhou Chen <Yezhou.chen@duke.edu>
2022-01-27 22:08:53 -0500

    adding task17

--- app/src/main/java/edu/duke/yc407/battleship/TextPlayer.java
+++ app/src/main/java/edu/duke/yc407/battleship/TextPlayer.java
@@ -2,13 +2,12 @@
 package edu.duke.yc407.battleship;
 
 import java.io.BufferedReader;
-import java.io.PrintStream;
 import java.io.IOException;
-import java.io.InputStream;
-import java.io.InputStreamReader;
 import java.io.PrintStream;
-import java.io.Reader;
-import java.io.StringReader;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.HashMap;
+import java.util.function.Function;
 
 public class TextPlayer {
   final Board<Character> theBoard;
@@ -17,6 +16,8 @@ public class TextPlayer {
   final PrintStream out;
   final AbstractShipFactory<Character> shipFactory;
   String name;
+  final ArrayList<String> shipsToPlace;
+  final HashMap<String, Function<Placement, Ship<Character>>> shipCreationFns;
 
   public TextPlayer(String name, Board<Character> theBoard, BufferedReader inputSource, PrintStream out,
       AbstractShipFactory<Character> shipFactory) {
@@ -26,6 +27,28 @@ public class TextPlayer {
     this.out = out;
     this.shipFactory = shipFactory;
     this.name = name;
+    //this.shipsToPlace = shipsToPlace;
+    //this.shipCreationFns = shipCreationFns;
+    this.shipsToPlace = new ArrayList<String>();
+    this.shipCreationFns = new HashMap<String, Function<Placement, Ship<Character>>>();
+    setupShipCreationMap();
+    setupShipCreationList();
+  }
+
+  /* put the String -> lambda mappings into shipCreationFns */
+  protected void setupShipCreationMap() {
+
+    shipCreationFns.put("Submarine", (p) -> shipFactory.makeSubmarine(p));
+    shipCreationFns.put("Destroyer", (p) -> shipFactory.makeSubmarine(p));
+    shipCreationFns.put("Battleships", (p) -> shipFactory.makeSubmarine(p));
+    shipCreationFns.put("Carriers", (p) -> shipFactory.makeSubmarine(p));
+  }
+
+  protected void setupShipCreationList() {
+    shipsToPlace.addAll(Collections.nCopies(2, "Submarine"));
+    shipsToPlace.addAll(Collections.nCopies(3, "Destroyer"));
+    shipsToPlace.addAll(Collections.nCopies(4, "Battleships"));
+    shipsToPlace.addAll(Collections.nCopies(6, "Carriers"));
   }
 
   public Placement readPlacement(String prompt) throws IOException {
@@ -34,19 +57,23 @@ public class TextPlayer {
     return new Placement(s);
   }
 
-  public void doOnePlacement() throws IOException {
+  public void doOnePlacement(String shipName, Function<Placement, Ship<Character>> createFn) throws IOException {
 
-    String s = "Player A Where would you like to put your ship?";
-    Ship<Character> ship = shipFactory.makeDestroyer(readPlacement(s));
-    theBoard.tryAddShip(ship);
+    // String s = "Player A Where would you like to put your ship?";
+    // Ship<Character> ship = shipFactory.makeDestroyer(readPlacement(s));
+    Placement p = readPlacement("Player " + name + " where do you want to place a " + shipName + "?");
+    Ship<Character> s = createFn.apply(p);
+    theBoard.tryAddShip(s);
     out.print(view.displayMyOwnBoard());
   }
 
   public void doPlacementPhase() throws IOException {
     out.print(view.displayMyOwnBoard());
-    String s = "Player A Where would you like to put your ship?";
-    //out.print(s);
-    doOnePlacement();
+    // String s = "Player A Where would you like to put your ship?";
+    for(int i = 0 ; i < shipsToPlace.size(); ++i){
+      doOnePlacement(shipsToPlace.get(i), shipCreationFns.get(shipsToPlace.get(i)));
+    }
+    // out.print(s);
 
   }
 
